28e4699a1d100db249b2eaf0fe217cf3e5501d4b
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2023-11-25 17:24:45 +0530

    Index route is defined

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PlaceList = require("./models/PlaceList.js")
+const path = require("path")
+
+
+//View engine set :-
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"));
 
 //Database creation :-
 
@@ -26,17 +32,24 @@ app.get("/", (req, res) => {
     res.send("Hi this is first response");
 })
 
-app.get("/testListing", async (req, res) => {
-    //New document creation
-    let SamplePlace = new PlaceList({
-        title: "My New Home",
-        description: "By the Sands",
-        price: 120000,
-        location: "Red Sea,Jordan",
-        country: "India",
-    })
+// Sample testing was done :-
 
-    await SamplePlace.save();
-    console.log("Sample was tested");
-    res.send("Sample testing is done");
-})
+// app.get("/testListing", async (req, res) => {
+//     //New document creation
+//     let SamplePlace = new PlaceList({
+//         title: "My New Home",
+//         description: "By the Sands",
+//         price: 120000,
+//         location: "Red Sea,Jordan",
+//         country: "India",
+//     })
+
+//     await SamplePlace.save();
+//     console.log("Sample was tested");
+//     res.send("Sample testing is done");
+// })
+
+app.get("/placelist",async (req,res) => {
+    const Places = await PlaceList.find({})
+    res.render("places/index.ejs",{Places})
+});
