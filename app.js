50657aea5912d8efe80e5173efe7e2531b79661c
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2023-11-26 20:18:26 +0530

    Error fixed of navbar styling

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")))
+
+//Major error fixed :-
+app.use('/placelist/:id',express.static(path.join(__dirname, "public")))
 app.engine("ejs",ejsMate);
 
 
