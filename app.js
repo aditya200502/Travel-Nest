6d7a77be3124648f0b4716c6c5250ece7f2eeaf5
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2023-11-26 17:23:58 +0530

    ejs-mate is installed and required

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const PlaceList = require("./models/PlaceList.js")
 const path = require("path")
 const methodOverride = require("method-override")
+const ejsMate = require("ejs-mate")
 
 
 //View engine set :-
