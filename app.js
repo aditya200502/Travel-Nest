554573cc55a2ab4f5bf2cb3c4651366edf33654e
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2023-12-31 17:08:57 +0530

    express  router is used to do some restructing

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ejsMate = require("ejs-mate")
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const Review = require("./models/Review.js")
-
+const placelist = require("./routes/placelist.js")
 
 
 //View engine set :-
@@ -90,70 +90,7 @@ app.delete("/placelist/:id/reviews/:reviewId",wrapAsync(async(req,res) => {
     res.redirect(`/placelist/${id}`);
 }))
 
-
-//Index route :-
-app.get("/placelist", wrapAsync(async (req, res) => {
-    const Places = await PlaceList.find({})
-    res.render("places/index.ejs", { Places })
-
-}));
-
-//Error was coming as new was also considered as an id
-
-//New Route :-
-app.get("/placelist/new", (req, res) => {
-    res.render("places/new.ejs");
-})
-//Create Route :-
-app.post("/placelist",wrapAsync(async (req, res, next) => {
-
-    //let{title,description,image,price,country,location} = req.body;
-    let placelist = req.body.placelist;
-
-    const newPlacelist = new PlaceList(placelist);
-    await newPlacelist.save();
-    res.redirect("/placelist");
-
-
-}))
-
-//Show route :-
-app.get("/placelist/:id", wrapAsync(async (req, res) => {
-
-    let { id } = req.params;
-
-    const place = await PlaceList.findById(id).populate("reviews");;
-    res.render("places/show.ejs", { place })
-}))
-
-
-//Edit Route :-
-app.get("/placelist/:id/edit",wrapAsync(async (req, res) => {
-
-    let { id } = req.params;
-    const place = await PlaceList.findById(id);
-    res.render("places/edit.ejs", { place });
-}))
-
-//Update and Save Route:-
-app.put("/placelist/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await PlaceList.findByIdAndUpdate(id, { ...req.body.placelist });
-    res.redirect(`/placelist/${id}`);
-}))
-
-//Delete Route :-
-app.delete("/placelist/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const deletedPlace = await PlaceList.findByIdAndDelete(id);
-    console.log(deletedPlace);
-    res.redirect("/placelist");
-}))
-
-//Match with every request :-
-app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page not Found!"));
-})
+app.use("/placelist",placelist)
 
 // Error handling middleware is being defined :-
 // It is made firstly for new route :-
@@ -162,5 +99,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", { err })
     //res.status(statusCode).send(message);
 })
+
 
 
---
+++ routes/placelist.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const PlaceList = require("../models/PlaceList.js")
+
+//Index route :-
+router.get("/", wrapAsync(async (req, res) => {
+    const Places = await PlaceList.find({})
+    res.render("places/index.ejs", { Places })
+
+}));
+
+//Error was coming as new was also considered as an id
+
+//New Route :-
+router.get("/new", (req, res) => {
+    res.render("places/new.ejs");
+})
+
+//Show route :-
+router.get("/:id", wrapAsync(async (req, res) => {
+
+    let { id } = req.params;
+
+    const place = await PlaceList.findById(id).populate("reviews");;
+    res.render("places/show.ejs", { place })
+}))
+
+//Create Route :-
+router.post("/",wrapAsync(async (req, res, next) => {
+
+    //let{title,description,image,price,country,location} = req.body;
+    let placelist = req.body.placelist;
+
+    const newPlacelist = new PlaceList(placelist);
+    await newPlacelist.save();
+    res.redirect("/placelist");
+
+
+}))
+
+//Edit Route :-
+router.get("/:id/edit",wrapAsync(async (req, res) => {
+
+    let { id } = req.params;
+    const place = await PlaceList.findById(id);
+    res.render("places/edit.ejs", { place });
+}))
+
+//Update and Save Route:-
+router.put("/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await PlaceList.findByIdAndUpdate(id, { ...req.body.placelist });
+    res.redirect(`/placelist/${id}`);
+}))
+
+//Delete Route :-
+router.delete("/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const deletedPlace = await PlaceList.findByIdAndDelete(id);
+    console.log(deletedPlace);
+    res.redirect("/placelist");
+}))
+
+//Match with every request :-
+router.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page not Found!"));
+})
+
+module.exports = router;
