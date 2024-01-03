6852acef84af7a9491956960d05b9148d7f50ae3
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2024-01-03 20:23:34 +0530

    controllers is created for placelist

---
+++ controllers/placelist.js
@@ -0,0 +1,81 @@
+const PlaceList = require("../models/PlaceList.js")
+
+//Index Route
+module.exports.indexRoute = async (req, res) => {
+    const Places = await PlaceList.find({})
+    res.render("places/index.ejs", { Places })
+
+}
+
+//New Route
+module.exports.newRoute = (req, res) => {
+    res.render("places/new.ejs");
+}
+
+//Show Route
+module.exports.showRoute = async (req, res) => {
+
+    let { id } = req.params;
+
+    const place = await PlaceList.findById(id)
+        .populate({
+            path: "reviews",
+            populate: { 
+                path: "author" 
+            }, 
+        })
+        .populate("owner")
+
+    if (!place) {
+        req.flash("error", "Listing which you have requested does not exists")
+        res.redirect("/placelist");
+    }
+    //console.log(place)
+    res.render("places/show.ejs", { place })
+}
+
+//Create Route
+module.exports.createRoute = async (req, res, next) => {
+
+    //let{title,description,image,price,country,location} = req.body;
+    let placelist = req.body.placelist;
+
+    const newPlacelist = new PlaceList(placelist);
+    newPlacelist.owner = req.user._id;
+    await newPlacelist.save();
+
+    req.flash("success", "New place is added")
+    res.redirect("/placelist");
+}
+
+//Edit Route
+module.exports.editRoute = async (req, res) => {
+
+    let { id } = req.params;
+    const place = await PlaceList.findById(id);
+
+    if (!place) {
+        req.flash("error", "Listing which you have requested does not exists")
+        res.redirect("/placelist");
+    }
+
+    res.render("places/edit.ejs", { place });
+}
+
+//Update and Save Route
+module.exports.updateRoute = async (req, res) => {
+    let { id } = req.params;
+    await PlaceList.findByIdAndUpdate(id, { ...req.body.placelist });
+    req.flash("success", "Placelist is updated")
+    res.redirect(`/placelist/${id}`);
+}
+
+//Delete Route
+module.exports.deleteRoute = async (req, res) => {
+    let { id } = req.params;
+
+    const deletedPlace = await PlaceList.findByIdAndDelete(id);
+    //console.log(deletedPlace);
+    req.flash("success", "Placelist is deleted")
+    res.redirect("/placelist");
+}
--- routes/placelistRouter.js
+++ routes/placelistRouter.js
@@ -4,91 +4,29 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const PlaceList = require("../models/PlaceList.js")
 const { isLoggedIn, isOwner } = require("../middleware.js")
+const {indexRoute,newRoute,showRoute, createRoute, editRoute} = require("../controllers/placelist.js")
 
 //Index route :-
-router.get("/", wrapAsync(async (req, res) => {
-    const Places = await PlaceList.find({})
-    res.render("places/index.ejs", { Places })
-
-}));
+router.get("/", wrapAsync(indexRoute));
 
 //Error was coming as new was also considered as an id
 
 //New Route :-
-router.get("/new", isLoggedIn, (req, res) => {
-
-    res.render("places/new.ejs");
-})
+router.get("/new", isLoggedIn, newRoute)
 
 //Show route :-
-router.get("/:id", wrapAsync(async (req, res) => {
-
-    let { id } = req.params;
-
-    const place = await PlaceList.findById(id)
-        .populate({
-            path: "reviews",
-            populate: { 
-                path: "author" 
-            }, 
-        })
-        .populate("owner")
-
-    if (!place) {
-        req.flash("error", "Listing which you have requested does not exists")
-        res.redirect("/placelist");
-    }
-    //console.log(place)
-    res.render("places/show.ejs", { place })
-}))
+router.get("/:id", wrapAsync(showRoute))
 
 //Create Route :-
-router.post("/", isLoggedIn, wrapAsync(async (req, res, next) => {
-
-    //let{title,description,image,price,country,location} = req.body;
-    let placelist = req.body.placelist;
-
-    const newPlacelist = new PlaceList(placelist);
-    newPlacelist.owner = req.user._id;
-    await newPlacelist.save();
-
-    req.flash("success", "New place is added")
-    res.redirect("/placelist");
-
-
-}))
+router.post("/", isLoggedIn, wrapAsync(createRoute))
 
 //Edit Route :-
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
-
-    let { id } = req.params;
-    const place = await PlaceList.findById(id);
-
-    if (!place) {
-        req.flash("error", "Listing which you have requested does not exists")
-        res.redirect("/placelist");
-    }
-
-    res.render("places/edit.ejs", { place });
-}))
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(editRoute))
 
 //Update and Save Route:-
-router.put("/:id", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await PlaceList.findByIdAndUpdate(id, { ...req.body.placelist });
-    req.flash("success", "Placelist is updated")
-    res.redirect(`/placelist/${id}`);
-}))
+router.put("/:id", isLoggedIn, isOwner, wrapAsync())
 
 //Delete Route :-
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-
-    const deletedPlace = await PlaceList.findByIdAndDelete(id);
-    //console.log(deletedPlace);
-    req.flash("success", "Placelist is deleted")
-    res.redirect("/placelist");
-}))
-
+router.delete("/:id", isLoggedIn, isOwner, wrapAsync())
 
 module.exports = router;
